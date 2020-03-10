61304b91be09426f57256d713bc26e916a5aba99
oneagustiranda <56112959+oneagustiranda@users.noreply.github.com>
2020-03-10 12:20:32 +0700

    Random Item
    Fungsi Random Item masih belum berfungsi dengan baik

---
+++ Assets/Scripts/RandomLoot/LootScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootScript : MonoBehaviour
+{
+    public GameObject coinObj;
+
+    [System.Serializable]
+    public class DropCurrency
+    {
+        public string name;
+        public GameObject item;
+        public int dropRarity;
+    }
+
+    public Animator myAnimator;
+    public int dropChance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<DropCurrency> LootTable = new List<DropCurrency>();
+
+    public void calculateLoot()
+    {
+        int calc_dropChance = Random.Range(0, 101);
+
+        if(calc_dropChance > dropChance)
+        {
+            Instantiate(coinObj, transform.position, Quaternion.identity);
+            Debug.Log("anda dapat coin");
+            return;
+        }else if (calc_dropChance <= dropChance)
+        {
+            int itemWeight = 0;
+
+            for(int i=0; i<LootTable.Count; i++)
+            {
+                itemWeight += LootTable[i].dropRarity;
+            }Debug.Log("itemWeight = " + itemWeight);
+
+            int randomValue = Random.Range(0, itemWeight);
+
+            for(int j=0;j<LootTable.Count; j++)
+            {
+                if(randomValue <= LootTable[j].dropRarity)
+                {
+                    Instantiate(LootTable[j].item, transform.position, Quaternion.identity);
+                    Debug.Log("anda dapat " + LootTable[j].name);
+                    return;
+                }
+                randomValue -= LootTable[j].dropRarity;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                myAnimator.SetTrigger("OpenChest");
+                calculateLoot();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RandomLoot/SpawnJump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnJump : MonoBehaviour
+{
+    public float upForce = 8f;
+    public float sideForce = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
+
+        Vector2 force = new Vector2(xForce, yForce);
+        GetComponent<Rigidbody2D>().velocity = force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
